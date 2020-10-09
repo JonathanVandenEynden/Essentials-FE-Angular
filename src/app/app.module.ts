ef6c81049f47626d7c048c75b27c94017cf96f2f
MarbodNaassens <47554942+MarbodNaassens@users.noreply.github.com>
2020-10-09 12:36:20 +0200

    Schermen aangemaakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChangeModule } from './change/change.module';
+import { UserModule } from './user/user.module';
+import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule
-  ],
+    ChangeModule,
+    UserModule,
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
