2ea6cafc02761a99024ec458cd99426b2aad593e
MarbodNaassens <47554942+MarbodNaassens@users.noreply.github.com>
2020-10-15 10:47:34 +0200

    Moving navigation to navigation module and moving recycled code to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './navigation/nav/nav.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ChangeModule } from './change/change.module';
 import { UserModule } from './user/user.module';
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavBarComponent } from './navigation/nav-bar/nav-bar.component';
+import {NavigationModule} from './navigation/navigation.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavComponent,
     PageNotFoundComponent,
   ],
   imports: [
+    NavigationModule,
     ChangeModule,
     UserModule,
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule
-    ],
+  ],
   providers: [],
+  exports: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/change/change.module.ts
+++ src/app/change/change.module.ts
@@ -11,6 +11,7 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { AddChangeComponent } from './add-change/add-change.component';
 import { ChangeButtonComponent } from './change-button/change-button.component';
 import {ChangeResolver} from './changeResolver';
+import {NavigationModule} from '../navigation/navigation.module';
 
 const routes: Routes =
 [
@@ -22,6 +23,7 @@ const routes: Routes =
 @NgModule({
   declarations: [RoadmapComponent, GroupComponent, SurveyComponent, HomeComponent, AddChangeComponent, ChangeButtonComponent ],
   imports: [
+    NavigationModule,
     CommonModule,
     HttpClientModule,
     ReactiveFormsModule,
