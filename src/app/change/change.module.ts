e10893f805db55cd7a16f7f24ecf710bce41f9e3
MarbodNaassens <47554942+MarbodNaassens@users.noreply.github.com>
2020-10-26 13:52:54 +0100

    Update screen startup

--- src/app/change/change.module.ts
+++ src/app/change/change.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RoadmapComponent } from './roadmap/roadmap.component';
-import { GroupComponent } from './group/group.component';
-import { SurveyComponent } from './survey/survey.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { AddChangeComponent } from './add-change/add-change.component';
-import { ChangeButtonComponent } from './change-button/change-button.component';
-import {ChangeResolver} from './changeResolver';
-import {NavigationModule} from '../navigation/navigation.module';
 import {LottieModule} from 'ngx-lottie';
+import {DeleteChangeComponent} from './delete-change/delete-change.component';
+import {UpdateChangeComponent} from './update-change/update-change.component';
 import {MaterialModule} from '../material/material.module';
-import { DeleteChangeComponent } from './delete-change/delete-change.component';
-import { UpdateChangeComponent } from './update-change/update-change.component';
+import {HomeComponent} from './home/home.component';
+import {AddChangeComponent} from './add-change/add-change.component';
+import {ChangeResolver} from './changeResolver';
+import {ChangeButtonComponent} from './change-button/change-button.component';
+import {NavigationModule} from '../navigation/navigation.module';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {SurveyComponent} from './survey/survey.component';
+import {GroupComponent} from './group/group.component';
+import {RoadmapComponent} from './roadmap/roadmap.component';
 
 const changeRoutes: Routes =
 [
   { path: 'home', component: HomeComponent },
   { path: 'add', component: AddChangeComponent},
-  { path: 'update', component: UpdateChangeComponent },
+  { path: 'update/:id', component: UpdateChangeComponent, resolve: { change : ChangeResolver } },
   { path: 'delete', component: DeleteChangeComponent},
   { path: 'survey/:id', component: SurveyComponent, resolve: { change : ChangeResolver }},
 ];
--- src/app/change/update-change/update-change.component.ts
+++ src/app/change/update-change/update-change.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ChangeInitiative} from '../change.model';
+import {faClipboard, faRoute, faUsers} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-update-change',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-change.component.css']
 })
 export class UpdateChangeComponent implements OnInit {
+  public change: ChangeInitiative;
+  faClip = faClipboard;
+  faGroup = faUsers;
+  faRoad = faRoute;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(item => this.change = item.change);
   }
 
 }
