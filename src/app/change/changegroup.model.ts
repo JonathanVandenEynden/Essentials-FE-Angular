cb3e270970c4a0d234d0790494d41ba46adb06c1
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-28 19:38:00 +0100

    add users to changegroup.model.ts

--- src/app/change/changegroup.model.ts
+++ src/app/change/changegroup.model.ts
@@ -1,18 +1,22 @@
+import {Employee} from './user.model';
 
 export interface ChangeGroupJson{
   id: number;
   name: string;
+  users: Employee[];
 }
 
 export class ChangeGroup {
   private ID: number;
   constructor(
     private NAME: string,
+    private USERS: Employee[]
   ) {}
 
   static fromJSON(json: ChangeGroupJson): ChangeGroup {
     const changegroup = new ChangeGroup(
       json.name,
+      json.users
     );
     changegroup.ID = json.id;
     return changegroup;
@@ -22,6 +26,7 @@ export class ChangeGroup {
     // @ts-ignore
     return {
       name: this.NAME,
+      users: this.USERS
     } as ChangeGroupJson;
   }
   get id(): number {
@@ -29,6 +34,9 @@ export class ChangeGroup {
   }
   get name(): string{
     return this.NAME;
+  }
+  get users(): Employee[]{
+    return this.USERS;
   }
 }
 
