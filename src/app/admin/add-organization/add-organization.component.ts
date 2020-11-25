7a6b66d6c88f6a40776ac0f6f026ff605d559346
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-11-25 15:59:11 +0100

    begin csv reading

--- src/app/admin/add-organization/add-organization.component.ts
+++ src/app/admin/add-organization/add-organization.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
+import {EmployeeCsvRecord} from './EmployeeCsvRecord';
+import {split} from 'ts-node';
 
 @Component({
   selector: 'app-add-organization',
@@ -9,8 +11,12 @@ import {Router} from '@angular/router';
 })
 export class AddOrganizationComponent implements OnInit {
   public organizationName: string;
+  public employeesList: EmployeeCsvRecord[];
 
-  constructor(private router: Router) { }
+  @ViewChild('csvReader') csvReader: any;
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
   }
@@ -18,4 +24,63 @@ export class AddOrganizationComponent implements OnInit {
   createOrganization(): void {
     // TODO
   }
+
+  convertCsv($event: any): void {
+    const text = [];
+    const files = $event.srcElement.files;
+
+    if (this.isValidCSVFile(files[0])) {
+
+      const input = $event.target;
+      const reader = new FileReader();
+      reader.readAsText(input.files[0]);
+
+      reader.onload = () => {
+        const csvData = reader.result;
+        const csvRecordsArray = ( csvData as string).split(/\r\n|\n/);
+
+        const headersRow = this.getHeaderArray(csvRecordsArray);
+
+        this.employeesList = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+      };
+    }
+  }
+
+  isValidCSVFile(file: any): any {
+    return file.name.endsWith('.csv');
+  }
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any): any {
+    const csvArr = [];
+
+    for (let i = 1; i < csvRecordsArray.length; i++) {
+      const currentRecord = (csvRecordsArray[i] as string).split(',');
+      if (currentRecord.length === headerLength) {
+        const csvRecord: EmployeeCsvRecord = new EmployeeCsvRecord();
+        csvRecord.name = currentRecord[0].trim();
+        csvRecord.country = currentRecord[1].trim();
+        csvRecord.office = currentRecord[2].trim();
+        csvRecord.factory = currentRecord[3].trim();
+        csvRecord.department = currentRecord[4].trim();
+        csvRecord.team = currentRecord[5].trim();
+        csvArr.push(csvRecord);
+      }
+    }
+    return csvArr;
+  }
+
+  getHeaderArray(csvRecordsArr: any): any {
+    const headers = (csvRecordsArr[0] as string).split(',');
+    const headerArray = [];
+    // tslint:disable-next-line:prefer-for-of
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+
+  fileReset(): void {
+    this.csvReader.nativeElement.value = '';
+    this.employeesList = [];
+  }
+
 }
