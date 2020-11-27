73029515dfe2853c3bf11e1e29ed0e9785765e09
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-11-27 11:31:02 +0100

    read csv to list of records

--- src/app/admin/add-organization/add-organization.component.ts
+++ src/app/admin/add-organization/add-organization.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 import {EmployeeCsvRecord} from './EmployeeCsvRecord';
-import {split} from 'ts-node';
 
 @Component({
   selector: 'app-add-organization',
@@ -11,76 +11,92 @@ import {split} from 'ts-node';
 })
 export class AddOrganizationComponent implements OnInit {
   public organizationName: string;
-  public employeesList: EmployeeCsvRecord[];
-
+  public employeeRecords: EmployeeCsvRecord[] = [];
   @ViewChild('csvReader') csvReader: any;
 
-  constructor(private router: Router) {
+
+  constructor(private router: Router, private http: HttpClient) {
   }
 
   ngOnInit(): void {
   }
 
-  createOrganization(): void {
-    // TODO
-  }
-
-  convertCsv($event: any): void {
+  onFileSelected(event): void {
     const text = [];
-    const files = $event.srcElement.files;
-
+    const files = event.target.files;
     if (this.isValidCSVFile(files[0])) {
 
-      const input = $event.target;
+      const input = event.target;
       const reader = new FileReader();
       reader.readAsText(input.files[0]);
 
       reader.onload = () => {
         const csvData = reader.result;
-        const csvRecordsArray = ( csvData as string).split(/\r\n|\n/);
+        const csvRecordsArray = (csvData as string).split(/\r\n|\n/);
 
         const headersRow = this.getHeaderArray(csvRecordsArray);
 
-        this.employeesList = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+        this.employeeRecords = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
       };
+
+      reader.onerror = () =>  {
+        console.log('error is occured while reading file!');
+      };
+
+    } else {
+      alert('Please import valid .csv file.');
+      this.fileReset();
     }
   }
 
-  isValidCSVFile(file: any): any {
-    return file.name.endsWith('.csv');
-  }
   getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any): any {
     const csvArr = [];
 
-    for (let i = 1; i < csvRecordsArray.length; i++) {
-      const currentRecord = (csvRecordsArray[i] as string).split(',');
-      if (currentRecord.length === headerLength) {
+    for (let i = 1; i < csvRecordsArray.length - 1; i++) {
+      const curruntRecord = (csvRecordsArray[i] as string).split(';');
+      if (curruntRecord.length === headerLength) {
         const csvRecord: EmployeeCsvRecord = new EmployeeCsvRecord();
-        csvRecord.name = currentRecord[0].trim();
-        csvRecord.country = currentRecord[1].trim();
-        csvRecord.office = currentRecord[2].trim();
-        csvRecord.factory = currentRecord[3].trim();
-        csvRecord.department = currentRecord[4].trim();
-        csvRecord.team = currentRecord[5].trim();
+        csvRecord.name = curruntRecord[0].trim();
+        csvRecord.country = curruntRecord[1].trim();
+        csvRecord.office = curruntRecord[2].trim();
+        csvRecord.factory = curruntRecord[3].trim();
+        csvRecord.department = curruntRecord[4].trim();
+        csvRecord.team = curruntRecord[5].trim();
         csvArr.push(csvRecord);
       }
     }
     return csvArr;
   }
 
+  isValidCSVFile(file: any): any {
+    return file.name.endsWith('.csv');
+  }
+
   getHeaderArray(csvRecordsArr: any): any {
-    const headers = (csvRecordsArr[0] as string).split(',');
+    const headers = (csvRecordsArr[0] as string).split(';');
     const headerArray = [];
-    // tslint:disable-next-line:prefer-for-of
     for (let j = 0; j < headers.length; j++) {
       headerArray.push(headers[j]);
     }
     return headerArray;
   }
 
-  fileReset(): void {
+  fileReset(): any {
     this.csvReader.nativeElement.value = '';
-    this.employeesList = [];
+    this.employeeRecords = [];
   }
 
+  onSubmit(): void {
+    console.log( this.employeeRecords);
+    // let json: any;
+    // json = {
+    //   organizationName: this.organizationName,
+    //   employees: this.selectedFile
+    //   // TODO changeManager
+    // };
+    // this.http.post(
+    //   `${environment.apiUrl}/Organizations`, json).subscribe((data: any) => {
+    //   console.log(data);
+    // });
+  }
 }
