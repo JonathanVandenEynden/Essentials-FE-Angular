a3718a97e9b1b1b4b6fc0062bdbe20fefb31113c
Sébastien De Pauw <sebastien.depauw@student.hogent.be>
2020-12-02 18:08:58 +0100

    Set up different types of charts
    Signed-off-by: Sébastien De Pauw <sebastien.depauw@student.hogent.be>

--- src/app/dashboard/chart/radar-chart/radar-chart.component.ts
+++ src/app/dashboard/chart/radar-chart/radar-chart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ChartDataSets, ChartType, RadialChartOptions} from 'chart.js';
+import {Label, MultiDataSet} from 'ng2-charts';
 
 @Component({
   selector: 'app-radar-chart',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radar-chart.component.css']
 })
 export class RadarChartComponent implements OnInit {
+  @Input() public radarChartProp: {t: string, d: ChartDataSets[], l: string[]};
+  public title: string;
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+  };
+  public radarChartLabels: Label[];
+  public radarChartData: ChartDataSets[];
+  public radarChartType: ChartType = 'radar';
+  public radarChartColors;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.title = this.radarChartProp.t;
+    this.radarChartLabels = this.radarChartProp.l;
+    this.radarChartData = this.radarChartProp.d;
+    const k: string[] = [];
+    for (let i = 0; i <= this.radarChartLabels.length; i++){
+      const n = this.random_rgba();
+      if (!k.includes(n)){
+        k.push(n);
+      }
+    }
+    this.radarChartColors = [{backgroundColor: k }];
   }
 
+  public random_rgba(): string {
+    const o = Math.round;
+    const r = Math.random;
+    const s = 255;
+    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + 0.5 + ')';
+  }
 }
