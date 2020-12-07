d96a25483774c21ebf2d4d039d332d1faa9a9572
Sébastien De Pauw <sebastien.depauw@student.hogent.be>
2020-12-07 17:14:28 +0100

    little problem fixed
    Signed-off-by: Sébastien De Pauw <sebastien.depauw@student.hogent.be>

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,8 +1,6 @@
 import { AuthenticationService } from './authentication.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import {map, tap} from 'rxjs/operators';
-import {Observable, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +10,7 @@ export class AuthGuard implements CanActivate {
   }
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): boolean {
-    if (this.authService.dummyUser$.getValue()) {
+    if (this.authService.user$) {
       return true;
     }
     this.authService.redirectUrl = state.url;
