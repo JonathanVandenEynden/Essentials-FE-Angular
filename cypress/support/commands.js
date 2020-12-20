d613a8cc06282c969d5f0f72fbc39f3a568a97c6
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2020-12-20 16:57:31 +0100

    invalid add Change form cypress test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,12 +24,4 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', () => {
-  const email = 'Sukrit.bhattacharya@essentials.com';
 
-  cy.request({
-    method: 'POST',
-    url: 'api/Account/login',
-    body: {email, password: 'P@ssword1'}
-  }).then((res) => localStorage.setItem('currentUser', res.body));
-});
